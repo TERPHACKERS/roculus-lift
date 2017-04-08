abb8395012879504e5e5bddcb94cdac19693d488
Ricky Han <rickylqhan@gmail.com>
2017-04-08 20:51:07 +0000

    setup

--- server.py
+++ server.py
@@ -1,21 +1,24 @@
-from flask import Flask
+from flask import Flask, Blueprint, render_template
 from flask_sockets import Sockets
 
+html = Blueprint('html', __name__,
+                 template_folder='templates')
 
 app = Flask(__name__)
+app.register_blueprint(html, url_prefix=r'/')
 sockets = Sockets(app)
 
-
-@sockets.route('/echo')
+@sockets.route('/acc')
 def echo_socket(ws):
     while not ws.closed:
         message = ws.receive()
+        print message
         ws.send(message)
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('/index.html')
 
 
 if __name__ == "__main__":
